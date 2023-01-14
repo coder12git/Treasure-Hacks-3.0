10b5d97819a6ef9c65a76076bd5ed0e18231b645
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-01-14 18:36:51 +0530

    small changes

--- pages/2_Demo.py
+++ pages/2_Demo.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import tensorflow as tf
+import PIL
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('./model/model.h5')
+    return model
+
 
 st.set_page_config(
     page_title="Alzheimer",
@@ -6,3 +14,47 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+st.title("Alzheimer's Disease Detection")
+st.write("This is a demo of the Alzheimer's Disease Detection app.")
+
+pic = st.file_uploader(
+    label="Upload an image",
+    type=["jpg", "png", "jpeg"],
+    accept_multiple_files=False,
+)
+
+if st.button("Predict"):
+    if pic is None:
+        st.write("Please upload an image file")
+    else:
+        st.header("Result")
+        cols = st.columns([1, 2])
+        with cols[0]:
+            st.image(pic, caption=pic.name, use_column_width=True)
+        with cols[1]:
+            model = load_model()
+            labels = [
+                "Mild Dementia",
+                "Moderate Dementia",
+                "No Dementia",
+                "Very Mild Dementia"
+            ]
+            with st.spinner("Predicting..."):
+                img = PIL.Image.open(pic)
+                img = img.resize((128, 128))
+                img = tf.keras.preprocessing.image.img_to_array(img)
+                img = tf.expand_dims(img, axis=0)
+
+                prediction = model.predict(img)
+                prediction = tf.nn.softmax(prediction)
+
+                score = tf.reduce_max(prediction) * 100
+
+                prediction = tf.argmax(prediction, axis=1)
+                prediction = prediction.numpy()[0]
+
+                result = labels[prediction]
+
+                st.write(f"Prediction: {result}")
+                st.write(f"Confidence: {score:.2f}%")
--- pages/3_Contact.py
+++ pages/3_Contact.py
@@ -10,16 +10,14 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Google Maps API key
-api_key = st.secrets["YOUR_API_KEY"]
 
 # Create a function to search for nearby doctors based on city name
-
-
-def search_doctors(city, api_key):
-    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query=doctors+specializing+in+Alzheimer+in+{city}&key={api_key}'
+def search_doctors(city):
+    # Google Maps API key
+    api_key = st.secrets["API_KEY"]
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=doctors+specializing+in+Alzheimer+in+{city}&key={api_key}"
     results = requests.get(url).json()
-    return results['results']
+    return results["results"]
 
 
 # Input city name
@@ -27,15 +25,17 @@ city = st.text_input("Enter city name: ")
 
 if city:
     # Search for nearby doctors
-    doctors = search_doctors(city, api_key)
+    doctors = search_doctors(city)
 
     # Display the results to the user
     st.write("Results for Alzheimer doctors in: ", city)
-    lat_long = [(doc['geometry']['location']['lat'], doc['geometry']
-                 ['location']['lng']) for doc in doctors]
+    lat_long = [
+        (doc["geometry"]["location"]["lat"], doc["geometry"]["location"]["lng"])
+        for doc in doctors
+    ]
     df = pd.DataFrame(lat_long, columns=["latitude", "longitude"])
     st.map(df)
 
     for i, doctor in enumerate(doctors):
-        st.write(f"{i+1}. Name: ", doctor['name'])
-        st.write("Address: ", doctor['formatted_address'])
+        st.write(f"{i+1}. Name: ", doctor["name"])
+        st.write("Address: ", doctor["formatted_address"])
