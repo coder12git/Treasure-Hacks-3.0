3ae6ef1c3df65c60f2149eb4cff67ff82308673f
Suruchi Kumari <suruchikumarimfp4@gmail.com>
2023-01-15 01:39:27 +0530

    Project complete

--- About.py
+++ About.py
@@ -34,43 +34,52 @@ st_lottie(lottie_welcome, height=450, key="welcome")
 
 with st.container():
     st.write("---")
+    st.write("##")
     st.header("What is Alzheimer?")
-    left_column, right_column = st.columns([2, 1])
+    st.write("##")
+    left_column, right_column = st.columns(2)
     with left_column:
+        st.write(
         """
         **Alzheimer** is known the most for the causes of **dementia**. It takes $2/3$ of the whole dementia population, while the cause is still unknown.
         According to papers from Lancet neurology, even the old theory such as neural inflations, which was disregarded as the cause compared to tau-protein, beta-amyloids, and genetic factors.
         The **World Health Organization** Trusted Source says that **47.5 million people** around the world are living with dementia.
         The **National Institutes of Health** estimate that more than **5 million people** in the United States have Alzheimer’s disease.
-        Although younger people can and do get Alzheimer’s, the symptoms generally begin after age 60.
+        Although younger people can and do get Alzheimer's, the symptoms generally begin after age 60.
+        There are degrees of severity in Alzheimer.
+        - **Very mildly demented**: This is the stage where patient starts to forget where they put their stuff, other people's names recently, etc. It is hard to detect through cognitive ability test.
+        - **Mildly demented**: This is the stage where patients don't remember the words, can't find their way to the destination, loss of focus and work-abilities. This is also the stage where patients even forget that they are losing memory. From this stage, with cognitive testing, it can be found.
+        - **Moderately demented**: Starts to forget the recent activities, important old histories, have hard time calculating the budget, hard to go outside alone, and loss of empathy.
+
+        There are 3 more stages in the moderately dementia, which in the terminal stage, the patient can't move on their own, while they lose the ability to speak.
+        Knowing these stages are important because the faster the stage the patient is at, the treatment will have higher effect in terms of slowing the process. If the dementia is found during the moderately demented stage, it is known that the patient will pass away in 3 years.
+        **Thus, having an AI that detects alzheimer dementia in the early stage can allow longer life expectancy from the patient as well as higher life quality overall from the slowdown of dementia.**
+        As Alzheimer can not only be found with cognitive ability testing, but also through MRI or CT by looking at the ventricles of the brain and cortical atrophy, the theoretical foundation on this project is solid. Doctors find the patient with Alzheimer's have a brain that have enlarged ventricles (that lies in the center of the brain) as well as thinner cortical grey area of the brain.
         """
+        )
+
+        st.write("##")
         st.write(
             "[**Learn More >**](https://en.wikipedia.org/wiki/Alzheimer%27s_disease)"
         )
     with right_column:
         st_lottie(lottie_health, key="check")
 
-    """
-    There are degrees of severity in Alzheimer.
-    - **Very mildly demented**: This is the stage where patient starts to forget where they put their stuff, other people's names recently, etc. It is hard to detect through cognitive ability test.
-    - **Mildly demented**: This is the stage where patients don't remember the words, can't find their way to the destination, loss of focus and work-abilities. This is also the stage where patients even forget that they are losing memory. From this stage, with cognitive testing, it can be found.
-    - **Moderately demented**: Starts to forget the recent activities, important old histories, have hard time calculating the budget, hard to go outside alone, and loss of empathy.
-
-    There are 3 more stages in the moderately dementia, which in the terminal stage, the patient can't move on their own, while they lose the ability to speak.
-    Knowing these stages are important because the faster the stage the patient is at, the treatment will have higher effect in terms of slowing the process. If the dementia is found during the moderately demented stage, it is known that the patient will pass away in 3 years.
-    **Thus, having an AI that detects alzheimer dementia in the early stage can allow longer life expectancy from the patient as well as higher life quality overall from the slowdown of dementia.**
-    As Alzheimer can not only be found with cognitive ability testing, but also through MRI or CT by looking at the ventricles of the brain and cortical atrophy, the theoretical foundation on this project is solid. Doctors find the patient with Alzheimer's have a brain that have enlarged ventricles (that lies in the center of the brain) as well as thinner cortical grey area of the brain.
-    """
-
 with st.container():
     st.write("---")
+    st.write("##")
     st.header("How it works?")
+    st.write("##")
     left_column, right_column = st.columns(2)
     with left_column:
+        st.write(
         """
         Our application utilizes machine learning to predict whether this person's brain image shows Alzheimer signs.
+        
         If the person have Alzheimer, than our application will also provide what stage the patient is, **it may be used as a supportive tool for doctors to diagnose Alzheimer's Disease**. It brings some light in the practical world as it may find early stages of AD, which can increase life expectancy as well as increased life quality with supportive treatments.
+
         We then recommend you specialized doctors in your nearby areas,based on your current location.
         """
+        )
     with right_column:
-        st_lottie(lottie_healthy, key="healthy")
+        st_lottie(lottie_healthy, height=300, key="healthy")
--- pages/4_Articles.py
+++ pages/4_Articles.py
@@ -37,24 +37,37 @@ with st.container():
         st.header("Alzheimer")
         """
         Alzheimer's disease is a progressive neurologic disorder that causes the brain to shrink (atrophy) and brain cells to die. Alzheimer's disease is the most common cause of dementia — a continuous decline in thinking, behavioral and social skills that affects a person's ability to function independently.
+
         Approximately 5.8 million people in the United States age 65 and older live with Alzheimer's disease. Of those, 80% are 75 years old and older. Out of the approximately 50 million people worldwide with dementia, between 60% and 70% are estimated to have Alzheimer's disease.
+
         The early signs of the disease include forgetting recent events or conversations. As the disease progresses, a person with Alzheimer's disease will develop severe memory impairment and lose the ability to carry out everyday tasks.
+
         Medications may temporarily improve or slow progression of symptoms. These treatments can sometimes help people with Alzheimer's disease maximize function and maintain independence for a time. Different programs and services can help support people with Alzheimer's disease and their caregivers.
+
         There is no treatment that cures Alzheimer's disease or alters the disease process in the brain. In advanced stages of the disease, complications from severe loss of brain function — such as dehydration, malnutrition or infection — result in death.
+        
         **Symptoms**
+
         Memory loss is the key symptom of Alzheimer's disease. Early signs include difficulty remembering recent events or conversations. As the disease progresses, memory impairments worsen and other symptoms develop.
+
         At first, a person with Alzheimer's disease may be aware of having difficulty remembering things and organizing thoughts. A family member or friend may be more likely to notice how the symptoms worsen.
+
         Brain changes associated with Alzheimer's disease lead to growing trouble with:
+
         - Memory
         - Thinking and reasoning
         - Making judgments and decisions
         - Planning and performing familiar tasks
         - Changes in personality and behavior
         - Preserved skills
+
         **When to see a doctor**
+
         A number of conditions, including treatable conditions, can result in memory loss or other dementia symptoms. If you are concerned about your memory or other thinking skills, talk to your doctor for a thorough assessment and diagnosis.
+
         If you are concerned about thinking skills you observe in a family member or friend, talk about your concerns and ask about going together to a doctor's appointment.
         """
+        st.write("##")
         st.write(
             "[**Learn More**](https://www.mayoclinic.org/diseases-conditions/alzheimers-disease/symptoms-causes/syc-20350447)"
         )
@@ -70,7 +83,6 @@ with st.container():
             st.write("##")
             st.write("##")
             st.image(img_alzheimer)
-            st.write("##")
             st.write("##")
             st.write("##")
             st.write("##")
